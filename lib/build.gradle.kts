a9987d7bbbb59d17d853c3d946946ecca81f152d
rhythmcache <153998419+rhythmcache@users.noreply.github.com>
2026-08-07 18:17:22 +0530

    Bump version from 1.0.3 to 1.0.5

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -29,7 +29,7 @@ publishing {
             from(components["java"])
             groupId = "com.github.rhythmcache"
             artifactId = "adb-kt"
-            version = "1.0.3"
+            version = "1.0.5"
 
             pom {
                 name.set("adb-kt")
