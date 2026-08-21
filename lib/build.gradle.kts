57f923576ce18df40e0ee6ad0610e52a9008b21d
rhythmcache <153998419+rhythmcache@users.noreply.github.com>
2026-08-22 00:59:58 +0530

    Update build.gradle.kts

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -29,7 +29,7 @@ publishing {
             from(components["java"])
             groupId = "com.github.rhythmcache"
             artifactId = "adb-kt"
-            version = "1.0.5"
+            version = "1.0.6"
 
             pom {
                 name.set("adb-kt")
